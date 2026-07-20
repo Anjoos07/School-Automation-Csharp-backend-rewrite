78eb08f8184fde5d62a1c90956af6f299f4a85be
Anjan <anjananjoos6826@gmail.com>
2026-07-20 22:43:15 +0530

    formOperation api added

--- obj/Debug/net10.0/worklinnEdu.AssemblyInfo.cs
+++ obj/Debug/net10.0/worklinnEdu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("worklinnEdu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57330757e35ef4de99b5ef89090cc387dd8145e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("worklinnEdu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("worklinnEdu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- routes/forms/formCreation.cs
+++ routes/forms/formCreation.cs
@@ -4,17 +4,37 @@ using System.Net.Http.Headers;
 
 namespace Forms;
 
-public static class FormCreation
+public static class FormModel
+{
+    public static async Task<IResult> Model(string url)
+    {
+        var baseUrl = url;
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "tly-2DX13TTyswPNQm3m0sRLJRFgDJpWtxlY");
+        var response = await client.GetAsync(baseUrl);
+        var json = await response.Content.ReadAsStringAsync();
+        return Results.Text(json, "application/json");
+    }
+}
+public static class FormOperations
 {
     public static void MapFormCreationEndpoints(this WebApplication app){
         app.MapGet("/form/{formId}", async (string formId) =>
         {
-            HeaderModel header = new HeaderModel("Bearer {api_key}");
-            var client = new HttpClient();
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "tly-2DX13TTyswPNQm3m0sRLJRFgDJpWtxlY");
-            var response = await client.GetAsync($"https://api.tally.so/forms/{formId}");
-            var json = await response.Content.ReadAsStringAsync();
-            return Results.Text(json, "application/json");
+            return await FormModel.Model($"https://api.tally.so/forms/{formId}");
+        });
+
+        app.MapGet("/form", async () =>
+        {
+            return await FormModel.Model($"https://api.tally.so/forms");
+        });
+        app.MapGet("/form-question/{formId}", async (string formId) =>
+        {
+            return await FormModel.Model($"https://api.tally.so/forms/{formId}/questions");
+        });
+        app.MapGet("/form-delete-submission/{formId}/{submissionId}",async (string formId, string submissionId) =>
+        {
+            return await FormModel.Model($"https://api.tally.so/forms/{formId}/submissions/{submissionId}");
         });
     }
 }
