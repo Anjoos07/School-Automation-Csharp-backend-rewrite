71e899be0c4998fe4d99f1097ac106a95f6df98f
Anjan <anjananjoos6826@gmail.com>
2026-07-23 19:49:44 +0530

    feat: changes made in fomrCreation

--- Components/tally/formCreation.cs
+++ Components/tally/formCreation.cs
@@ -3,8 +3,17 @@ using System.Security.Cryptography.X509Certificates;
 using Utilities;
 
 namespace Forms;
-public static class FormPostModel
+public static class FormModel
 {
+    public static async Task<IResult> GetModel(string url)
+    {
+        var baseUrl = url;
+        var client = new HttpClient();
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "tly-2DX13TTyswPNQm3m0sRLJRFgDJpWtxlY");
+        var response = await client.GetAsync(baseUrl);
+        var json = await response.Content.ReadAsStringAsync();
+        return Results.Text(json, "application/json");
+    }
      // For Post Operations
     public class Payload
     {
--- routes/forms/formOperations.cs
+++ routes/forms/formOperations.cs
@@ -4,20 +4,6 @@ using System.Net.Http.Headers;
 
 
 namespace Forms;
-public static class FormGetModel
-{
-    // For Get Operations
-    public static async Task<IResult> GetModel(string url)
-    {
-        var baseUrl = url;
-        var client = new HttpClient();
-        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "tly-2DX13TTyswPNQm3m0sRLJRFgDJpWtxlY");
-        var response = await client.GetAsync(baseUrl);
-        var json = await response.Content.ReadAsStringAsync();
-        return Results.Text(json, "application/json");
-    }
-
-}
 
 
 public static class FormOperations
@@ -25,19 +11,19 @@ public static class FormOperations
     public static void MapFormOperationsEndpoints(this WebApplication app){
         app.MapGet("/form/{formId}", async (string formId) =>
         {
-            return await FormGetModel.GetModel($"https://api.tally.so/forms/{formId}");
+            return await FormModel.GetModel($"https://api.tally.so/forms/{formId}");
         });
         app.MapGet("/form", async () =>
         {
-            return await FormGetModel.GetModel($"https://api.tally.so/forms");
+            return await FormModel.GetModel($"https://api.tally.so/forms");
         });
         app.MapGet("/form-question/{formId}", async (string formId) =>
         {
-            return await FormGetModel.GetModel($"https://api.tally.so/forms/{formId}/questions");
+            return await FormModel.GetModel($"https://api.tally.so/forms/{formId}/questions");
         });
         app.MapGet("/form-delete-submission/{formId}/{submissionId}",async (string formId, string submissionId) =>
         {
-            return await FormGetModel.GetModel($"https://api.tally.so/forms/{formId}/submissions/{submissionId}");
+            return await FormModel.GetModel($"https://api.tally.so/forms/{formId}/submissions/{submissionId}");
         });
     }
 }
