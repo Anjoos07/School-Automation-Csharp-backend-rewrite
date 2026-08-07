bea1b089404cf891fd4fa013db8ed0247084d315
Navaneeth0101 <navaneeth2726@gmail.com>
2026-08-08 02:24:57 +0530

    merge

--- obj/Debug/net10.0/worklinnEdu.AssemblyInfo.cs
+++ obj/Debug/net10.0/worklinnEdu.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("worklinnEdu")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22c5cfc791f1c67cca25c381e2f1e2241a87d276")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed7293efb120692148a445da47febfd794f3a330")]
 [assembly: System.Reflection.AssemblyProductAttribute("worklinnEdu")]
 [assembly: System.Reflection.AssemblyTitleAttribute("worklinnEdu")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
